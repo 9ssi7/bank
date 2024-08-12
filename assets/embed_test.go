c862ff55b3207eb5b3a69c2c1476366d8972acaf
Sami Salih İbrahimbaş <contact@9ssi7.dev>
2024-08-13 02:01:35 +0300

    first commit

---
+++ assets/embed_test.go
@@ -0,0 +1,23 @@
+package assets
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func TestEmbedMailTemplate(t *testing.T) {
+	// Get the embedded FS
+	fs := EmbedMailTemplate()
+
+	for i := 0; i < reflect.ValueOf(Templates).NumField(); i++ {
+		templateName := reflect.ValueOf(Templates).Type().Field(i).Name
+		path := reflect.ValueOf(Templates).Field(i).String()
+		t.Run(templateName, func(t *testing.T) {
+			_, err := fs.Open(fmt.Sprintf("mail/%s.html", path))
+			if err != nil {
+				t.Errorf("Failed to open embedded template %s: %v", templateName, err)
+			}
+		})
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/9ssi7/bank
+
+go 1.22.0
